cfa35d2c79cf264353143974294ddb3cf0480ae7
khanhdo05 <khanhphuongdo28@gmail.com>
2024-11-20 18:07:20 -0600

    format

--- src/main/java/edu/grinnell/csc207/blockchains/BlockChain.java
+++ src/main/java/edu/grinnell/csc207/blockchains/BlockChain.java
@@ -21,8 +21,7 @@ public class BlockChain implements Iterable<Transaction> {
   /**
    * Create a new blockchain using a validator to check elements.
    *
-   * @param check
-   *   The validator used to check elements.
+   * @param check The validator used to check elements.
    */
   public BlockChain(HashValidator check) {
     // STUB
@@ -37,16 +36,14 @@ public class BlockChain implements Iterable<Transaction> {
   // +---------+
 
   /**
-   * Mine for a new valid block for the end of the chain, returning that
-   * block.
+   * Mine for a new valid block for the end of the chain, returning that block.
    *
-   * @param t
-   *   The transaction that goes in the block.
+   * @param t The transaction that goes in the block.
    *
    * @return a new block with correct number, hashes, and such.
    */
   public Block mine(Transaction t) {
-    return new Block(10, t, new Hash(new byte[] {7}), 11);       // STUB
+    return new Block(10, t, new Hash(new byte[] {7}), 11); // STUB
   } // mine(Transaction)
 
   /**
@@ -55,14 +52,13 @@ public class BlockChain implements Iterable<Transaction> {
    * @return the number of blocks in the chain, including the initial block.
    */
   public int getSize() {
-    return 2;   // STUB
+    return 2; // STUB
   } // getSize()
 
   /**
    * Add a block to the end of the chain.
    *
-   * @param blk
-   *   The block to add to the end of the chain.
+   * @param blk The block to add to the end of the chain.
    */
   public void append(Block blk) {
     // STUB
@@ -71,12 +67,11 @@ public class BlockChain implements Iterable<Transaction> {
   /**
    * Attempt to remove the last block from the chain.
    *
-   * @return false if the chain has only one block (in which case it's
-   *   not removed) or true otherwise (in which case the last block
-   *   is removed).
+   * @return false if the chain has only one block (in which case it's not removed) or true
+   *         otherwise (in which case the last block is removed).
    */
   public boolean removeLast() {
-    return true;        // STUB
+    return true; // STUB
   } // removeLast()
 
   /**
@@ -85,35 +80,33 @@ public class BlockChain implements Iterable<Transaction> {
    * @return the hash of the last sblock in the chain.
    */
   public Hash getHash() {
-    return new Hash(new byte[] {2, 0, 7});   // STUB
+    return new Hash(new byte[] {2, 0, 7}); // STUB
   } // getHash()
 
   /**
-   * Determine if the blockchain is correct in that (a) the balances are
-   * legal/correct at every step, (b) that every block has a correct
-   * previous hash field, (c) that every block has a hash that is correct
-   * for its contents, and (d) that every block has a valid hash.
+   * Determine if the blockchain is correct in that (a) the balances are legal/correct at every
+   * step, (b) that every block has a correct previous hash field, (c) that every block has a hash
+   * that is correct for its contents, and (d) that every block has a valid hash.
    *
    * @return true if the blockchain is correct and false otherwise.
    */
   public boolean isCorrect() {
-    return true;        // STUB
+    return true; // STUB
   } // isCorrect()
 
   /**
-   * Return an iterator of all the people who participated in the
-   * system.
+   * Return an iterator of all the people who participated in the system.
    *
    * @return an iterator of all the people in the system.
    */
   public Iterator<String> users() {
     return new Iterator<String>() {
       public boolean hasNext() {
-        return false;   // STUB
+        return false; // STUB
       } // hasNext()
 
       public String next() {
-        throw new NoSuchElementException();     // STUB
+        throw new NoSuchElementException(); // STUB
       } // next()
     };
   } // users()
@@ -121,13 +114,12 @@ public class BlockChain implements Iterable<Transaction> {
   /**
    * Find one user's balance.
    *
-   * @param user
-   *   The user whose balance we want to find.
+   * @param user The user whose balance we want to find.
    *
    * @return that user's balance (or 0, if the user is not in the system).
    */
   public int balance(String user) {
-    return 0;   // STUB
+    return 0; // STUB
   } // balance()
 
   /**
@@ -138,11 +130,11 @@ public class BlockChain implements Iterable<Transaction> {
   public Iterator<Block> blocks() {
     return new Iterator<Block>() {
       public boolean hasNext() {
-        return false;   // STUB
+        return false; // STUB
       } // hasNext()
 
       public Block next() {
-        throw new NoSuchElementException();     // STUB
+        throw new NoSuchElementException(); // STUB
       } // next()
     };
   } // blocks()
@@ -155,11 +147,11 @@ public class BlockChain implements Iterable<Transaction> {
   public Iterator<Transaction> iterator() {
     return new Iterator<Transaction>() {
       public boolean hasNext() {
-        return false;   // STUB
+        return false; // STUB
       } // hasNext()
 
       public Transaction next() {
-        throw new NoSuchElementException();     // STUB
+        throw new NoSuchElementException(); // STUB
       } // next()
     };
   } // iterator()
